8e2e26913a0b697b2a97382ab1d4ba6992f57fb7
PABLOSleuth2 <opsnonman@gmail.com>
2026-07-11 22:57:34 +0500

    Update Emote.lua

--- Emote.lua
+++ Emote.lua
@@ -1,1 +1,17 @@
-{}
+{
+  ["Not my problem"] = {
+    ["New"] = true,
+    ["SecondMusic"] = true,
+    ["categoryid"] = 2,
+    ["color"] = Color3.fromRGB(8, 0, 255),
+    ["endposition"] = 13,
+    ["id"] = 123536753943350,
+    ["idr6"] = 129698175871918,
+    ["music"] = "rbxassetid://95367830729666",
+    ["music2"] = "rbxassetid://83482292414648",
+    ["musicspeed"] = 0.8,
+    ["name"] = "[⭐] Not my problem",
+    ["speed"] = 1,
+    ["timeposition"] = 2
+    }
+}
