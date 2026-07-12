0eacdd8adab5447c1b5d5611c55186e9a51db541
PABLOSleuth2 <opsnonman@gmail.com>
2026-07-12 17:07:23 +0500

    Update Emote.lua

--- Emote.lua
+++ Emote.lua
@@ -13,5 +13,20 @@
     ["name"] = "[⭐] Not my problem",
     ["speed"] = 1,
     ["timeposition"] = 2
+    },
+  ["sadraddin-baqyt"] = {
+    ["New"] = true,
+    ["SecondMusic"] = true,
+    ["categoryid"] = 1,
+    ["color"] = Color3.fromRGB(0, 255, 247),
+    ["endposition"] = 48.8,
+    ["id"] = 81217869654335,
+    ["idr6"] = 102371043154005,
+    ["music"] = "rbxassetid://132063707761772",
+    ["music2"] = "rbxassetid://93658285353017",
+    ["musicspeed"] = 0.8,
+    ["name"] = "[🇰🇿] Sadraddin baqyt",
+    ["speed"] = 1,
+    ["timeposition"] = 2
     }
 }
